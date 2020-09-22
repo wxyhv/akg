88059f7e453d8809d70e0ba7758dccda2573c69e
zhengzuohe <zhengzuohe@huawei.com>
2020-09-22 21:14:55 +0800

    fix dsl bugs and reorder test cases of fused ops.

--- tests/operators/gpu/test_fused_pattern_grad.py
+++ tests/operators/gpu/test_fused_pattern_grad.py
@@ -20,33 +20,29 @@ def relu_grad_np(head, in_data):
     return np.where(in_data > 0, head, 0)
 
 def bn_beta_grad_np(head, layout='NHWC'):
-    if layout == 'NHWC':
-        n, h, w, c = head.shape
-    elif layout == 'NCHW':
-        n, c, h, w = head.shape
-    else:
+    if layout == 'NCHW':
+        head = topi.transpose(head, (0, 2, 3, 1))
+    elif layout != "NHWC":
         raise NotImplementedError(
             'layout is not supported {} '.format(layout)
         ) 
     
-    reshape = np.reshape(head, (n * h * w, c))
-    bn_beta_grad = np.sum(reshape, axis=0)
+    bn_beta_grad = np.sum(head, axis=(0, 1, 2))
     return bn_beta_grad
 
 def bn_gamma_grad_np(head, in_data, data_sum, layout='NHWC'):
-    if layout == 'NHWC':
-        n, h, w, c = head.shape
-    elif layout == 'NCHW':
-        n, c, h, w = head.shape
-    else:
+    if layout == 'NCHW':
+        head = topi.transpose(head, (0, 2, 3, 1))
+    elif layout != "NHWC":
         raise NotImplementedError(
             'layout is not supported {} '.format(layout)
         )
 
-    mean = np.divide(data_sum, n * c * h)
-    x_hat = np.subtract(in_data, data_sum)
+    n, h, w, c = head.shape
+    mean = np.divide(data_sum, n * h * w)
+    x_hat = np.subtract(in_data, mean)
     x_hat_mul = np.multiply(x_hat, head)
-    bn_gamma_grad = np.sum(np.reshape(x_hat_mul, (n * h * w, c)), axis=0)
+    bn_gamma_grad = np.sum(x_hat_mul, axis=(0, 1, 2))
     return bn_gamma_grad
 
     
